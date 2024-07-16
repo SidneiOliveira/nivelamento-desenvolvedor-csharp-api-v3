77125582debe0dd1157da0b071694eb41a3aab8b
Sidnei Oliveira <prodeveloper.sidnei@gmail.com>
2024-07-16 19:13:03 -0300

    feat(2): add 'FootballRequest|FootballDto|FootballResponse' into Questao2 application | update 'Program'

---
+++ Questao2/FootballDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Questao2
+{
+    public class FootballDto
+    {
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public string Team1goals { get; set; }
+        public string Team2goals { get; set; }
+        public int Year { get; set; }
+    }
+}
