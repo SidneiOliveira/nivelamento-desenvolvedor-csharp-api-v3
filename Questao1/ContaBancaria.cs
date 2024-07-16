b0f271ad849801419df394528db0908da5106b52
Sidnei Oliveira <prodeveloper.sidnei@gmail.com>
2024-07-16 12:49:09 -0300

    feat(1): update ContaBancaria into Questao1 application

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,29 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    public class ContaBancaria
+    {
+        public int Numero { get; private init; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        public ContaBancaria(int numero, string titular, double depositoInicial = 0)
+        {
+            Numero = numero;
+            Titular = titular;
+            Deposito(depositoInicial);
+        }
 
-       
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            double taxa = 3.5;
+            quantia += taxa;
+            Saldo -= quantia;
+        }
+        public override string ToString() => $"Conta: {Numero}, Titular: {Titular}, Saldo: {string.Format("{0:C2}", Saldo, CultureInfo.InvariantCulture)}".ToString();
     }
 }
